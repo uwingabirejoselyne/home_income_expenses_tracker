aabc77920a86a07f461f3678e1e224f85642bf79
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-08-12 20:13:31 +0200

    setting the server

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+
+require('dotenv').config()
+
+const PORT = process.env.PORT
+
+app.use(express.json());
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.send('hello world')
+    
+})
+const server = ()=>{
+app.listen(PORT,()=>{
+    console.log('You are listening to port:',PORT)
+})
+}
+server()
