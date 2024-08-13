cebb44a716ccc84a814ddfcf00fdd760ccc9a783
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-08-13 11:49:41 +0200

    adding validation

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -2,11 +2,15 @@ const Income = require('../models/incomeModel');
 const asyncHandler = require("express-async-handler");
 
 const addIncome = asyncHandler(async (req, res) => {
+    const {title,amount,date,category,description } = req.body
+    if(!title || !amount || !date || !category|| !description){
+        return res.status(400).json({message:'All fields are required'});
+    }
     try {
         const newIncome = await Income.create(req.body);
-        res.json(newIncome);
+        res.status(201).json(newIncome);
       } catch (error) {
-        throw new Error(error);
+        res.status(500).json({ message: 'Failed to create income', error: error.message });
       }
  
 })
