cc473c9e5ae5df790374b47f8f92a939865f5296
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-08-13 09:59:53 +0200

    creation of transaction route

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express()
 const db = require('./db/db');
+const transactionRoute = require('./routes/transactionRoute')
 
 require('dotenv').config()
 
@@ -9,6 +10,7 @@ const PORT = process.env.PORT
 
 app.use(express.json());
 app.use(cors())
+app.use('/transaction',transactionRoute);
 
 app.get('/',(req,res)=>{
     res.send('hello world')
