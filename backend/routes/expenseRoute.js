e089386a1281a8e616ff847824b905637e3163ed
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-08-15 11:59:47 +0200

    added delete  expenses endpoint

--- backend/routes/expenseRoute.js
+++ backend/routes/expenseRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const { addExpenses, getExpense } = require('../controllers/expenseController');
+const { addExpenses, getExpense, deletedExpense } = require('../controllers/expenseController');
 const router = express.Router();
 router.post('/',addExpenses)
 router.get('/get-expenses',getExpense)
+router.delete('/:id/delete-expense',deletedExpense)
 module.exports = router
