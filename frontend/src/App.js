e761eb752baee10ed6457d284e3832dad231c75c
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-08-17 22:05:01 +0200

    added images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
-
+import styled from "styled-components";
+import avatar from './img/bg.png'; 
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <AppStyled bg={avatar}> 
+    </AppStyled>
   );
 }
+
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url(${props => props.bg});
+`;
 
 export default App;
--- frontend/src/styles/GlobalStyle.js
+++ frontend/src/styles/GlobalStyle.js
@@ -1,22 +1,25 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-  * {
+  *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     list-style: none;
   }
-:root{
-  --primary-color:#22260;
-  --primary-color:'color:rgba(34,34,96,.6)';
-  --primary-color:'color:rgba(34,34,96,.4)';
-  --color-grey:#aaa;
-  --primary-accent:#f56692;
-  --primary-delete:#ff0000;
-}
-  body{
-  font-family:'Nunito',sans-serif';
-  background-color:red;
+
+  :root {
+    --primary-color: rgba(34, 34, 96, 1);
+    --primary-color-light: rgba(34, 34, 96, 0.6);
+    --primary-color-lighter: rgba(34, 34, 96, 0.4);
+    --color-grey: #aaa;
+    --primary-accent: #f56692;
+    --primary-delete: #ff0000;
+  }
+
+  body {
+    font-family: 'Nunito', sans-serif;
+    background-color: black;
   }
 `;
+
