94edb2512db888393cb897ba3a3e831e26a68e96
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-08-19 08:56:26 +0200

    layout creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 import avatar from './img/bg.png'; 
+import { MainLayout } from "./styles/Layout";
 
 function App() {
   return (
-    <AppStyled bg={avatar}> 
+    <AppStyled bg={avatar}>
+      <MainLayout>
+        <h1>Hello</h1>
+      </MainLayout>
     </AppStyled>
   );
 }
@@ -11,6 +15,7 @@ function App() {
 const AppStyled = styled.div`
   height: 100vh;
   background-image: url(${props => props.bg});
+  position:relative
 `;
 
 export default App;
---
+++ frontend/src/styles/Layout.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const MainLayout = styled.div`
+padding:2rem;
+height:100%;
+display:flex;
+gap:
+`
+
+export const InnerLayout = styled.div`
+padding: 2rem 1.5 rem;
+width: 100%;
+`
