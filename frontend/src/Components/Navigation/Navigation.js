10290c7a67ff356e4fe94c8e1a4b966f55a5723a
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-08-20 11:20:10 +0200

    created menu items

--- frontend/src/Components/Navigation/Navigation.js
+++ frontend/src/Components/Navigation/Navigation.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
+import avatar from '../../img/avatar.png'
 
 const Navigation = () => {
     const NavStyled = styled.nav``
-  return (
-    <NavStyled>
-      
-    </NavStyled>
-  )
+    return (
+        <NavStyled>
+            <div className="user-icon">
+                <img src={avatar} alt='avatar'/>
+                <p className="text">
+                    <h2>Joselyne</h2>
+                    <p>Your money</p>
+                </p>
+            </div>
+            
+
+        </NavStyled>
+    )
 }
 
 export default Navigation
---
+++ frontend/src/utils/menuItems.js
@@ -0,0 +1,20 @@
+export const menuItems = [
+    {
+        id: 1,
+        title: 'Dashboard',
+        icon: '',
+        link: '/dashboard'
+    },
+    {
+        id: 2,
+        title: 'View transaction',
+        icon: transactions,
+        link: '/dashboard'
+    },
+    {
+        id: 2,
+        title: 'Incomes',
+        icon: trend,
+        link: '/dashboard'
+    }
+]
