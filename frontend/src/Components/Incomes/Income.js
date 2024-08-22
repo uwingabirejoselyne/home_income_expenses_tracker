a8e3de47453057ffd91dec23334ef657ee70089a
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-08-22 09:23:49 +0200

    created income component

---
+++ frontend/src/Components/Incomes/Income.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Income = () => {
+    const IncomeStyled = styled.div``
+  return (
+    <IncomeStyled>
+      
+    </IncomeStyled>
+  )
+}
+
+export default Income
