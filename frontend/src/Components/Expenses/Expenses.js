9e349f94db8abda573942724434cbc6be6b9727b
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-08-22 09:26:08 +0200

    created expenses component

---
+++ frontend/src/Components/Expenses/Expenses.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Expenses = () => {
+    const ExpenseStyled = styled.div``
+  return (
+    <ExpenseStyled>
+      
+    </ExpenseStyled>
+  )
+}
+
+export default Expenses
